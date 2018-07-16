fe6532cd6dbe3c0238ac40262d4aec0818532dc4
Dolph Mathews <dolph.mathews@gmail.com>
2018-07-16 16:54:29 -0500

    Generate a token at startup

--- main.go
+++ main.go
@@ -1,17 +1,34 @@
 package main
 
 import "fmt"
+import "math/rand"
 import "log"
+import "time"
 import "net/http"
 
 import "github.com/gorilla/mux"
+import "github.com/ernsheong/grand"
 
 func main() {
+	// Seed random number generator for token & ID generation.
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	token := NewToken()
+	fmt.Println("Initial token:", token.Id)
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index).Methods("GET")
 	router.HandleFunc("/reservations", CreateReservation).Methods("POST")
 	router.HandleFunc("/reservations/{reservationId}", GetReservation).Methods("GET")
 	router.HandleFunc("/reservations/{reservationId}", DeleteReservation).Methods("DELETE")
+
 	fmt.Println("Listening on localhost:8080...")
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func NewToken() Token {
+	generator := grand.NewGenerator(grand.CharSetBase62)
+	token := Token{Id: generator.GenerateRandomString(8)}
+	tokens[token.Id] = &token
+	return token
 }
